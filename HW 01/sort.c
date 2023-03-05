8e04e52cd9ba7b97a6459208d861a39551c328bf
Miller11k <89824650+Miller11k@users.noreply.github.com>
2023-03-05 09:32:09 -0500

    Added Comments

--- HW 01/sort.c
+++ HW 01/sort.c
@@ -3,17 +3,21 @@
 #include "sort.h"
 
 void ssort(int * arr, int size) {
-	
+// initialize variables
 	int i;
 	int j;
 	int temp = 0;
+
 	#ifdef ASCENDING
+	// sort in ascending order
 	for(i = 0; i < size; i++)
 	{
 		for(j = i + 1; j < size; j++)
 		{
+			// check if current element is greater than next element
 			if(arr[i] > arr[j])
 			{
+				// swap elements if current element is greater than next element
 				temp = arr[i];
 				arr[i] = arr[j];
 				arr[j] = temp;
@@ -21,17 +25,19 @@ void ssort(int * arr, int size) {
 		}
 	}
 	#else
+	// sort in descending order
 	for(i = 0; i < size; i++)
 	{
 		for(j = i + 1; j < size; j++)
 		{
+			// check if current element is less than next element
 			if(arr[i] < arr[j])
 			{
+				// swap elements if current element is less than next element
 				temp = arr[i];
 				arr[i] = arr[j];
 				arr[j] = temp;
 			}
 		}
 	}
-	#endif
-}
+#endif
